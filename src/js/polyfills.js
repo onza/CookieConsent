23c4696bb893cd617b08df02d7ffa2be1659f859
robindanzinger <robindanzinger@freenet.de>
2020-02-14 14:18:09 +0100

    add polyfill for dom node remove

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -2,3 +2,20 @@ import 'core-js/features/array/from';
 import 'core-js/features/array/includes'; 
 import 'core-js/features/string/starts-with';
 import 'core-js/features/string/includes';
+
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode !== null)
+          this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
