e65043192de3242e8fc2e304f4a38f31b1345f6f
onza <info@martinfarkas.com>
2023-03-28 10:57:43 +0200

    kicked out

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/js/polyfills.js',
-  output: {
-    filename: 'polyfills.js',
-    path: path.resolve(__dirname, 'lib/js'),
-  },
-};
