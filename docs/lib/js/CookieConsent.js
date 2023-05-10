d8df1cb11a01b8b96d50fdd8803a5f7725e360c1
onza <info@martinfarkas.com>
2023-05-10 09:34:16 +0200

    generated files

--- docs/lib/js/CookieConsent.js
+++ docs/lib/js/CookieConsent.js
@@ -33,6 +33,7 @@ window.cookieconsent = {
           <label class="cc-banner_settings_label" for="option1">Marketing / Analyse</label>
         </div>
         <p>
+          Unter Performance- und Marketing-Cookies verstehen wir technisch nicht notwendige Cookies.
           Wir verwenden Performance und Marketing Cookies nur, wenn Sie uns zuvor Ihre Einwilligung gegeben haben.
           Mit solchen Cookies erfassen wir Informationen darüber, wie Besucher mit unserer Webseite interagieren und welche Seiten sie besucht haben.
           Dies hilft uns, die Benutzeraktivitäten auf unserer Website sowohl auf aggregierter als auch auf persönlicher Ebene zu verstehen,
@@ -87,6 +88,8 @@ const cookieconsent_cookie_fn = {
   }
 }
 
+
+// Pardot - start -
 function disablePardot () {
   deleteScriptIncluding("pardot.com");
 }
@@ -115,8 +118,10 @@ function enablePardot () {
     var c = document.getElementsByTagName('script')[0]; 
     c.parentNode.insertBefore(s, c);
 }
+// Pardot - end -
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs
+
+// Google Analytics - start -
 function enableGooglesitetag () {
   window['ga-disable-' + window.cookieconsent.googlesitetag.gt_id] = false;
 
@@ -135,19 +140,17 @@ function enableGooglesitetag () {
     window.dataLayer.push(arguments);
   }
   gtag('js', new Date());
-  gtag('config', window.cookieconsent.googlesitetag.gt_id, { 'anonymize_ip': true });
+  gtag('config', window.cookieconsent.googlesitetag.gt_id);
   window.cookieconsent.googlesitetag.script_loaded = true;
 }
 
 function disableGooglesitetag () {
-  // disable google analytics, existing cookies will not be deleted
-  // existing cookies will neither be updated nor sent to google
-  // see: https://developers.google.com/analytics/devguides/collection/gtagjs/user-opt-out
   window['ga-disable-' + window.cookieconsent.googlesitetag.gt_id] = true;
-  // if we remove the script and then the user activates the cookies, the script will not be added to dom
-  // we have to add the script manually. Therefore we don't delete the script
 }
+// Google Analytics - end  -
 
+
+// Youtube - start -
 function showVideo(contentElementId) {
   var video_parent_div = document.getElementById(contentElementId)
   var video_iframe = video_parent_div.querySelector('iframe');
@@ -177,6 +180,9 @@ function disableVideo() {
     hideVideo(video_parent_div.id);
   });
 }
+// Youtube - end -
+
+
 
 /*
  * ==========================
@@ -240,7 +246,7 @@ function showCookieConsentSettings() {// eslint-disable-line no-unused-vars
     const cookie = window.cookieconsent.options[key];
     var lang = document.documentElement.lang;
     const checked = cookie.allow ? 'checked' : '';
-    checkboxes += `<div class="form-check"><input class='js-cookieconsent__checkbox' type='checkbox' value='${key}' ${checked}> ${cookie['text_' + lang]}`
+    checkboxes += `<div class="form-check"><input class='js-cookieconsent_checkbox' type='checkbox' value='${key}' ${checked}> ${cookie['text_' + lang]}`
   })
   if (document.documentElement.lang === 'en') {
     consentBanner.innerHTML = `    
@@ -274,7 +280,7 @@ function showCookieConsentSettings() {// eslint-disable-line no-unused-vars
             <label class="cc-banner_settings_label-required" for="technical-required-cookies">Technisch erforderliche Cookies - standardmäßig immer ausgewählt</label>
           </div>
           <p>Unter technisch erforderlichen Cookies verstehen wir Cookies, ohne die die technische Bereitstellung des Online-Angebots nicht gewährleistet werden kann. Dazu gehören z.B. Cookies, die wesentliche Dienste wie eine reibungslose Wiedergabe von Video- oder Audioaufnahmen unterstützen. Dieser Kategorie werden auch so genannte "funktionale Cookies" zugeordnet. Funktionale Cookies speichern Informationen, um Ihnen eine komfortable Nutzung unserer Online-Services (z.B. Sprachauswahl) zu ermöglichen. Rechtsgrundlage für die Verarbeitung personenbezogener Daten mit Hilfe von Cookies dieser Kategorie sind berechtigte Interessen von uns. Hierzu gehören u.a. das Interesse an einer professionellen Außendarstellung sowie an einer optimalen Verteilung der Lasten auf dem Server aus technischen Gründen.</p>
-          <a class="cc-banner_more" href="http://www.domain-xyz.com/datenschutz/" title="Link to Cookie Informations">More details</a>
+          <a class="cc-banner_more" href="http://www.domain-xyz.com/datenschutz/" title="Link to Cookie Informations">Weitere Informationen</a>
           ${checkboxes}
         </div>
         <button class="cc-banner_settings-btn" onclick='allowOnly()'>Auswahl bestätigen</button>
@@ -342,7 +348,7 @@ function denyAll() {// eslint-disable-line no-unused-vars
 }
 
 function allowOnly() {// eslint-disable-line no-unused-vars
-  let value = "allowOnly:" + Array.from(document.getElementsByClassName("js-cookieconsent__checkbox"))
+  let value = "allowOnly:" + Array.from(document.getElementsByClassName("js-cookieconsent_checkbox"))
     .filter(el => el.checked)
     .map(el => el.value)
     .join(',')
